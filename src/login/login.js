61cfe1a00c03789257324af8c365b5f59ba246db
Katta Pratheek Reddy (RBEI/BSL7) <tkt2kor@bosch.com>
2020-08-25 17:00:54 +0530

    logic added to login page

--- src/login/login.js
+++ src/login/login.js
@@ -1,27 +1,58 @@
 import React from "react";
+import axios from 'axios'
+
 import './login.css'
 const login=(props)=>{
+    let email=''
+    let otp=''
+    let header=''
+    let updateEmail=(value)=>{
+        email=value
+        // console.log(email)
+    }
+
+    let updateOtp=(value)=>{
+        otp=value
+    }
+
+    let getOtp=()=>{
+        console.log('getotp')
+        const post = axios.post('https://0appkh5ipbo57270um-rbei-njs-forum.cfapps.eu10.hana.ondemand.com/getotp',{
+            email
+        });
+        post.then((result) => {
+            console.log(result)
+            alert(result.data.status)
+        }).catch((e) => {
+          console.log(e)
+        })
+    }
+
+    let validateOtp=()=>{
+        console.log('validate otp')
+        const post = axios.post('https://0appkh5ipbo57270um-rbei-njs-forum.cfapps.eu10.hana.ondemand.com/login',{
+            email,otp
+        });
+        post.then((result) => {
+            alert(result.data.status)
+            if(result.data.status==='valid otp'){
+                header=result.data.token
+            }
+            console.log(header)
+        }).catch((e) => {
+          console.log(e)
+        })
+    }
+
     return (
-<div className = 'form'>
-        <form action='/login' method='post'>
-            <form action="/login" method="post">
-                <ul>
-                    <li>
-                        <label for="mail">E-mail:</label>
-                        <input type="email" id="mail" name="user_email"/>
-                        <button>Get OTP</button>
-                    </li>
-                    <li>
-                        <label for="otp">OTP:</label>
-                        <input type='text' placeholder="Enter your OTP" id="otp" name="otp" />
-                    </li>
-                    <li class="button">
-                        <button type="submit">Login</button>
-                    </li>
-                </ul>
-            </form>
-        </form>
-    </div>
+            <div className = 'form'>
+                <label >E-mail:</label>
+                <input  type="text"  name="user_email" onChange={event=>updateEmail(event.target.value)}/>
+                <button onClick={getOtp}>Get OTP</button><br/>
+                <label >OTP:</label>
+                <input type='text' placeholder="Enter your OTP" id="otp" name="otp" onChange={event=>updateOtp(event.target.value)}/><br/>
+                <button onClick={validateOtp}>Login</button>
+            </div>
     )
 }
 
