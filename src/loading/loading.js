09ee041b17a5154f7db0b585040716e499f3ae42
pratheek reddy <kattapratheek97@gmail.com>
2020-09-25 11:38:25 +0530

    loading fixed

--- src/loading/loading.js
+++ src/loading/loading.js
@@ -1,16 +1,18 @@
 import React from "react";
 // import loading from '../fonts/loading.gif'
-import './loading.css'
-const Loading=()=>{
-    return(
-        // <img className='img' src={loading} alt='loading...'></img>
-        // <div className="loader" tabIndex='-2'></div>
-        <div className="spinner" tabIndex='-2'>
-            <div className="bounce1"></div>
-            <div className="bounce2"></div>
-            <div className="bounce3"></div>
-        </div>
-    )
-}
+import "./loading.css";
+const Loading = () => {
+  return (
+    // <img className='img' src={loading} alt='loading...'></img>
+    // <div className="loader" tabIndex='-2'></div>
+    <div className="loader">
+      <div className="spinner">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
